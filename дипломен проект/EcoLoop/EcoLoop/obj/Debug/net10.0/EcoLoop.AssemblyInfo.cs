fa4d8dbf65c6ab48f1d12d19719da5f9fbaaf2e5
kcekov3 <kcekov3@gmail.com>
2026-02-17 19:51:10 +0200

    add

--- дипломен проект/EcoLoop/EcoLoop/obj/Debug/net10.0/EcoLoop.AssemblyInfo.cs
+++ дипломен проект/EcoLoop/EcoLoop/obj/Debug/net10.0/EcoLoop.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EcoLoop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4ba1bd0abbd0619791f5394ceddf482162065b27")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b87a8beaa63ba168432aa94cd69d4031da1b49d6")]
 [assembly: System.Reflection.AssemblyProductAttribute("EcoLoop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EcoLoop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
